5d529f4f33fb0ae6f915e38754b02a73124bd76d
karinfdez <karinfdez@yahoo.com>
2018-11-12 18:36:19 -0500

    Add reduxtagram router with components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,24 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom'; 
+
+//Import css
+import css from './styles/style.styl';
+
+
+import Main from './components/Main'; 
+import Single from './components/Single'; 
+import PhotoGrid from './components/PhotoGrid'; 
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+    <Router history={browserHistory}>
+       <Route path="/" component={Main}>    {/* If it match the "/", grab the Main component */}
+            <IndexRoute component={PhotoGrid}></IndexRoute>  {/* Depending on the url structure, either pass Main and Photogrid, or Main with Single.
+                                                            Photogrid and Single are children of the Main component */}
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+);
+
+render(router, document.getElementById('root'));
